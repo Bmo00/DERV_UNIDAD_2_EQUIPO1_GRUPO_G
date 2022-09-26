084517df4da8fa7c8587fa5594c372c979ca9eb7
Quebec <luuisman07f@hotmail.com>
2022-09-25 20:27:55 -0500

    Proyecto Agregado

---
+++ Assets/Scripts/SingletonUsuario.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonUsuario : MonoBehaviour
+{
+
+
+    public static SingletonUsuario instancia;
+    public int vida = 70;
+    public int ataque = 20;
+
+
+    private void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
